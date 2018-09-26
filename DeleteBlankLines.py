1db0ec8ca5b431aac452a61e50b666ad8c3dbc51
Daniel Pérez <djdany.djesus@gmail.com>
2018-09-26 08:32:58 +0200

    Refactored
    Refactored and better documented.

--- DeleteBlankLines.py
+++ DeleteBlankLines.py
@@ -2,10 +2,20 @@ import pyperclip
 import sys
 
 
-def get_lines(raw_file, start):
+# -----------------------------------------------------
+def delete_blank_lines(raw_file, start):
+    """
+    Opens the file, checks than file has more lines than start,
+    go through the file deleting the blank lines and return it.
+    """
     with open(raw_file, 'r', encoding='utf-8') as file:
+        file_length = len(file.readlines())
         clean_file = ''
         c = 0
+
+        if (file_length < start):
+            raise Exception()
+
         for line in file:
             c += 1
             if c >= start:
@@ -15,23 +25,33 @@ def get_lines(raw_file, start):
         return clean_file
 
 
+# -----------------------------------------------------
 def clipboard(to_clipboard):
+    """Simply copy the string apssed to clipboard"""
     pyperclip.copy(to_clipboard)
 
 
+# -----------------------------------------------------
 def main():
+    """Gets the arguments passed, cleans the file and copy it to the clipboard."""
     try:
         raw_file = sys.argv[1]
     except:
-        print('File is necessary')
-
-    if len(sys.argv) == 3:
-        start = sys.argv[2]
+        print('You need to pass a file for the first argument.')
     else:
-        start = 0
+        try:
+            start = int(sys.argv[2])
+        except:
+            start = 0
+        finally:
+            try:
+                clean_file = delete_blank_lines(raw_file, start)
+            except:
+                print('The file is shorter than start.')
+            else:
+                clipboard(clean_file)
+                print('Succesfully copied to clipboard.')
 
-    clean_file = get_lines(raw_file, start)
-    clipboard(clean_file)
 
-
-main()
+if (__name__ == "__main__"):
+    main()
